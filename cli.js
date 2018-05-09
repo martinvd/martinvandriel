a63c1f9d378a13451ae242b0c1a7c04388cff1ca
Martin van Driel <martinvd@gmail.com>
2018-05-10 00:00:09 +0200

    Init

---
+++ cli.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+console.log(`
+  Name: Martin van Driel
+  Email: martinvd@gmail.com
+  Website: https://martinvandriel.com
+  Twitter: https://twitter.com/martinvandriel
+  GitHub: https://github.com/martinvd
+`);
---
+++ readme.md
@@ -0,0 +1,15 @@
+# martinvandriel
+
+> The [Martin van Driel](https://martinvandriel.com) CLI
+
+## Usage
+
+Install Node.js, then:
+
+```
+$ npx martinvandriel
+```
+
+## License
+
+MIT © [Martin van Driel](https://martinvandriel.com)
